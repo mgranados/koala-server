623f52088ce0094ba767a22a7accfdbbd49e5ee1
Martin Granados <margrangr@gmail.com>
2018-10-27 19:29:18 -0500

    added initial middlewares

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
+const compress = require('koa-compress');
+const responseTime = require('koa-response-time');
+const logger = require('koa-logger');
 
 const app = new Koa();
+app.use(bodyParser());
+app.use(compress());
+app.use(responseTime());
+app.use(logger());
 
 app.use(async (ctx) => {
   ctx.body = 'Sweet baby Jesus';
